7ff113c2ac6c4e835ca240110667e547a2fece81
unknown <ibmjavanh15@iiht.tech>
2021-04-20 15:31:39 +0530

    Added counter to keep check on remaining characters

--- src/app/bug/bug.component.ts
+++ src/app/bug/bug.component.ts
@@ -5,32 +5,52 @@ import { BugService } from '../bugService';
 @Component({
   selector: 'app-bug',
   templateUrl: './bug.component.html',
-  styleUrls: ['./bug.component.css','./style.css']
+  styleUrls: ['./bug.component.css', './style.css'],
 })
 export class BugComponent implements OnInit {
-  bug:Bug = new Bug();  //model
-  ImgPath:string="./assets/bughawk.jpeg";
-  constructor(private bugService: BugService) { }
+  bug: Bug = new Bug(); //model
+  ImgPath: string = './assets/bughawk.jpeg';
+  constructor(private bugService: BugService) {}
 
   bugArray: Bug[] = [];
+  maxLengthSynopsis = 50;
+  maxLengthDescription = 200;
 
-  create(){
+  create() {
     console.log(this.bug.name);
     const promise = this.bugService.create(this.bug);
-    promise.subscribe(response => {
-      console.log(response);
-      this.bug.id = response;
-      this.bugArray.push(Object.assign({},this.bug));
-      alert("Bug added!");
-      this.bug = new Bug();
-    },
-    error => {
-    console.log(error);
-    alert("Error occurred!")
-    })
+    promise.subscribe(
+      (response) => {
+        console.log(response);
+        this.bug.id = response;
+        this.bugArray.push(Object.assign({}, this.bug));
+        alert('Bug added!');
+        this.bug = new Bug();
+      },
+      (error) => {
+        console.log(error);
+        alert('Error occurred!');
+      }
+    );
   }
 
-  ngOnInit(): void {
+  valueCheckSynopsis() {
+    const remainingCharactersSynopsis = <HTMLTextAreaElement>(
+      document.getElementById('charSynopsis')
+    );
+    length = this.bug.synopsis.length;
+    length = this.maxLengthSynopsis - length;
+    remainingCharactersSynopsis.textContent = length.toString();
   }
 
+  valueCheckDescription() {
+    const remainingCharactersDescription = <HTMLTextAreaElement>(
+      document.getElementById('charDescription')
+    );
+    length = this.bug.description.length;
+    length = this.maxLengthDescription - length;
+    remainingCharactersDescription.textContent = length.toString();
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/searchbug/searchbug.component.ts
+++ src/app/searchbug/searchbug.component.ts
@@ -5,55 +5,65 @@ import { BugService } from '../bugService';
 @Component({
   selector: 'app-searchbug',
   templateUrl: './searchbug.component.html',
-  styleUrls: ['./searchbug.component.css','./style.css']
+  styleUrls: ['./searchbug.component.css', './style.css'],
 })
 export class SearchbugComponent implements OnInit {
   bug: Bug = new Bug();
-  ImgPath:string="./assets/bughawk.jpeg";
+  ImgPath: string = './assets/bughawk.jpeg';
   constructor(private bugService: BugService) {}
 
   bugArray: any;
+  maxLength = 20;
 
-  getBugsByName(name: String){
-    if(name){
-      if(name.trim()){
+  getBugsByName(name: String) {   //method to retrieve bugs by name
+    if (name) {
+      if (name.trim()) {
         const observable = this.bugService.getBugsByName(name);
-        observable.subscribe(response => {
-        this.bugArray = response
-        },
-        error => {
-          console.log(error);
-          alert("Error!")
-          })
-        }
-        else{
-          alert("No such record found!")
-        }
-    }
-    else{
-      alert("Please enter bug name!")
+        observable.subscribe(
+          (response) => {
+            this.bugArray = response;
+          },
+          (error) => {
+            console.log(error);
+            alert('Error!');
+          }
+        );
+      } else {
+        alert('No such record found!');
+      }
+    } else {
+      alert('Please enter bug name!');
     }
   }
 
-  getBugsByStatus(status:any){
-    if(status){
+  getBugsByStatus(status: any) {    //method to retrieve bugs by status
+    if (status) {
       const observable = this.bugService.getBugsByStatus(status);
-      observable.subscribe(response => {
-        this.bugArray = response
-      },
-      error => {
-        console.log(error);
-        alert("No such record found!")
-        })
+      observable.subscribe(
+        (response) => {
+          this.bugArray = response;
+        },
+        (error) => {
+          console.log(error);
+          alert('No such record found!');
+        }
+      );
+    } else {
+      alert('Please select status!');
     }
-    else{
-      alert("Please select status!")
-    }
+  }
+
+  valueCheck() {    //method to keep check on characters remaining
+    const remainingCharacters = <HTMLInputElement>(
+      document.getElementById('charName')
+    );
+    length = this.bug.name.length;
+    length = this.maxLength - length;
+    remainingCharacters.textContent = length.toString();
   }
 
   ngOnInit(): void {
     const observable = this.bugService.getAllBugs();
-    observable.subscribe(response => this.bugArray = response);
+    observable.subscribe((response) => (this.bugArray = response));
   }
-
 }
